bb25b0a790259be23758316e287aef28d0888662
sudarshan233 <nsridharsudarshan@gmail.com>
2025-12-17 19:56:44 +0530

    Removed path from server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,22 +2,11 @@ import express from 'express';
 import cors from 'cors';
 
 import router from "./routes/weather.routes";
-import path from "path";
 
 const app = express();
-const __dirname = path.resolve()
 
 app.use(cors());
 app.use(express.json());
-
-if(process.env.NODE_ENV !== 'production') {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-    })
-
-}
 
 app.use("/api/weather", router);
 
